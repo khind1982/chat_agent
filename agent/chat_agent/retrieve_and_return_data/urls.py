9c8981eeca2bd9cb1a28139506b642c9e00c15ae
Kevin Hind <kevin.hind@clarivate.com>
2023-03-23 17:06:53 +0000

    Implemented code to enable users to answer two questions in the chat agent.

--- agent/chat_agent/retrieve_and_return_data/urls.py
+++ agent/chat_agent/retrieve_and_return_data/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('retrieve_and_return_data/', views.retrieve_data, name='retrieve_data')
+    path('retrieve_and_return_data/', views.RetrieveData.as_view(), name='home')
 ]
--- agent/chat_agent/retrieve_and_return_data/views.py
+++ agent/chat_agent/retrieve_and_return_data/views.py
@@ -7,34 +7,70 @@ from django.template import loader
 from django.template import RequestContext
 
 from retrieve_and_return_data.forms import LazyActiveForm
+from retrieve_and_return_data.forms import TemperatureRatingForm
+
+from multi_form_view import MultiModelFormView
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 
 from chat_agent_utils import retrieve_holiday_data
 
-def retrieve_data(request):
-    context = {}
-    form = LazyActiveForm()
-    context['form'] = form
-    if request.method == 'POST':
+class RetrieveData(MultiModelFormView):
+    form_classes = {
+        'lazy_form' : LazyActiveForm,
+        'climate_form' : TemperatureRatingForm
+    }
+    template_name = 'chat_interface.html'
+    def get_success_url(self):
+        return reverse('home')
+    def forms_valid(self, forms):
+        activity = forms['lazy_form'].save(commit=False)
+        climate = forms['climate_form'].save(commit=False)
+        return super(RetrieveData, self).forms_valid(forms)
+    def get(self, request, *args, **kwargs):
+        return self.render_to_response(self.get_context_data())
+    def post(self, request, **kwargs):
+        context = {}
         lazy = request.POST.getlist('lazy')
         active = request.POST.getlist('active')
-        if lazy:
-            results = retrieve_holiday_data([('lazy'), ('mild'), ('city')])
+        cold = request.POST.getlist('cold')
+        mild = request.POST.getlist('mild')
+        hot = request.POST.getlist('hot')
+        request = render_database_request(lazy, active, cold, mild, hot)
+        results = retrieve_holiday_data(request)
+        reformatted_results = reformat_database_results(results)
+        if reformat_database_results(results):
+            print('results')
             context['results'] = reformat_database_results(results)
-        if active:
-            results = retrieve_holiday_data([('active'), ('mild'), ('city')])
-            context['results'] = reformat_database_results(results)
-    return render(
-        request, "chat_interface.html", context
-    )
+        else:
+            print('default')
+            context['default'] = results[0]
+        return self.render_to_response(context)
+
+
+def render_database_request(lazy, active, cold, mild, hot):
+    request = []
+    activity_options = {'lazy':lazy, 'active':active}
+    for activity_option,value in activity_options.items():
+        if value:
+            request.append((activity_option))
+    
+    climate_options = {'cold':cold, 'mild':mild, 'hot':hot}
+    for climate_option,value in climate_options.items():
+        if value:
+            request.append((climate_option))
+    request.append(('city'))
+    return request
 
 
 def reformat_database_results(results):
     reformatted_results = []
     for result in results:
         reformatted_result = {}
-        city, price, country, hotel = result
+        try:
+            city, price, country, hotel = result
+        except ValueError:
+            return False
         reformatted_result['city'] = city
         reformatted_result['price'] = price
         reformatted_result['country'] = country
